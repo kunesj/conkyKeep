b7d227873e40862fa3f44a5ccfe052147f52a44c
Jiří Kuneš <jirka642@gmail.com>
2016-06-02 11:18:57 +0200

    updated googlekeep parsing after site layout upgrade

--- conkyKeep/session_google.py
+++ conkyKeep/session_google.py
@@ -12,35 +12,35 @@ import json
 class SessionGoogle:
     """
     Google Login code is based on http://stackoverflow.com/a/24881998
-    
-    googleKeep_data - Notes from GoogleKeep    
+
+    googleKeep_data - Notes from GoogleKeep
     googleKeep_data['id']       - id of note
     googleKeep_data['parentId'] - id of parent "note"
     googleKeep_data['title']    - title of note
     googleKeep_data['text']     - text inside of note
-    googleKeep_data['color']    - color of note 
+    googleKeep_data['color']    - color of note
     data['serverId']
     data['timestamps']
     data['labelIds']
     data['baseVersion']
     data['abuseFeedback']
     data['errorStatus']
-    data['kind'] 
-    data['checked']  
-    data['type'] 
-    data['shareRequests'] 
-    data['nodeSettings']   
-    data['reminders'] 
-    data['sortValue'] 
-    data['roleInfo'] 
-    data['parentServerId'] 
+    data['kind']
+    data['checked']
+    data['type']
+    data['shareRequests']
+    data['nodeSettings']
+    data['reminders']
+    data['sortValue']
+    data['roleInfo']
+    data['parentServerId']
     data['isArchived']
     +other
     """
-    
+
     googleKeep_data_raw = []
     googleKeep_data = []
-    
+
     def __init__(self, login, pwd, url_login="https://accounts.google.com/ServiceLogin", url_auth="https://accounts.google.com/ServiceLoginAuth"):
         self.ses = requests.session()
         login_html = unicode(self.ses.get(url_login).text)
@@ -56,30 +56,30 @@ class SessionGoogle:
 
     def get(self, URL):
         return self.ses.get(URL).text
-        
+
     def googleKeep_getNotes(self, raw=False):
         html = self.get("https://keep.google.com/")
         bs = BeautifulSoup(unicode(html))
-        script = bs.body.findAll('script')[1].text
-        script_loadChunk = script.split(';')[0]
-        
+        script = bs.body.findAll('script')[0].text
+        script_loadChunk = script.split(';')[-2]
+
         # fill self.googleKeep_data_raw
         data = script_loadChunk.split("loadChunk(JSON.parse('")[1]
         data = "'), ".join(data.split("'), ")[:-1])
-        
+
         # convert \x?? charcters
         while data.find('\\x') != -1:
             index = data.find('\\x')
             hex_str = data[index:index+4]
             val = int(data[index+2:index+4], 16)
             data = data.replace(hex_str, chr(val))
-        
+
         # remove redundant \
         data = data.replace('\\\\','\\')
-        
+
         # decode json string
         self.googleKeep_data_raw = json.loads(data) # encodes string to utf-8 ?
-        
+
         if not raw:
             self.googleKeep_data = []
             for data in self.googleKeep_data_raw:
@@ -87,44 +87,44 @@ class SessionGoogle:
                 trashed = data['timestamps']['trashed']
                 if trashed != '1970-01-01T00:00:00.000Z':
                     continue
-                
+
                 data['text'] = data['text'].encode('utf-8')
                 self.googleKeep_data.append(data)
-            
+
             # create note tree
             self.googleKeep_data = self.googleKeep_getNotesTree(self.googleKeep_data)
         else:
             self.googleKeep_data = self.googleKeep_data_raw
-            
+
         return self.googleKeep_data
-    
+
     def googleKeep_getNotesTree(self, notes):
         """
         Autocalled by googleKeep_getNotes
         """
-        root_notes = []        
+        root_notes = []
         for cn in notes:
             # get root notes
             if cn['parentId'] == 'root':
                 root_notes.append(cn)
                 continue
-            
+
             # add child notes to parent notes
             for pn in notes:
                 if cn['parentId'] == pn['id']:
                     if 'childNotes' not in pn:
                         pn['childNotes'] = []
                     pn['childNotes'].append(cn)
-                            
+
         return root_notes
-    
+
     def googleKeep_formatNotes(self, notes, child=False):
         """
         requires 'childNotes' in note data
         """
         # create copy of notes before modifications
         notes = copy.deepcopy(notes)
-        
+
         for rn in notes:
             # recursivelly format child notes
             if 'childNotes' in rn:
@@ -133,36 +133,36 @@ class SessionGoogle:
             if 'text' not in rn:
                 rn['text'] = ""
             rn['formatedText'] = rn['text']
-            
+
             ## Root type notes
-            if rn['type'] == "NOTE": # if text note (root note type)              
+            if rn['type'] == "NOTE": # if text note (root note type)
                 # add formated text from child notes
                 for cn in childNotes:
                     if rn['formatedText'] != "":
                         rn['formatedText'] += "\n"
                     rn['formatedText'] += cn['formatedText']
-            
+
             elif rn['type'] == "LIST": # if List note (root note type)
                 for cn in childNotes:
                     if rn['formatedText'] != "":
                         rn['formatedText'] += "\n"
-                    
+
                     if cn['checked']:
                         rn['formatedText'] += "[*] "
                     else:
                         rn['formatedText'] += "[ ] "
                     rn['formatedText'] += cn['formatedText']
-            
+
             ## Child type notes
             elif rn['type'] == 'LIST_ITEM': # if text note or list item
                 rn['formatedText'] = rn['text']
-                
+
             elif rn['type'] == 'BLOB': # if image note
                 rn['formatedText'] = "[BLOB mime='"+rn['blob']['mimetype']+"' url='https://keep.google.com/media/"+rn['blob']["media_id"]+"']"
-                
+
             else:
                 print "Unknown NoteType:"+rn['type']+" not implemented!"
-        
+
         # remove unneeded values and add missing default values
         if not child:
             formated_notes = []
@@ -174,7 +174,7 @@ class SessionGoogle:
                     'type': "NOTE",
                     'id': 0
                 }
-                
+
                 if 'color' in n:
                     _note['color'] = n['color']
                 if 'title' in n:
@@ -183,11 +183,11 @@ class SessionGoogle:
                     _note['type'] = n['type']
                 if 'id' in n:
                     _note['id'] = n['id']
-                
-                formated_notes.append(_note)             
+
+                formated_notes.append(_note)
         else:
             formated_notes = notes
-                
+
         return formated_notes
 
 if __name__ == "__main__":
@@ -195,7 +195,7 @@ if __name__ == "__main__":
     if len(sys.argv)!=3:
         print "ERROR: Bad number of arguments"
         sys.exit(2)
-            
+
     session = SessionGoogle(str(sys.argv[1]), str(sys.argv[2]))
     notes = session.googleKeep_getNotes()
     notes = session.googleKeep_getNotesTree(notes)
